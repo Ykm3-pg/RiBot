2d15ff1531b7a6d86479e4d3e0c59aa0a62a6658
ykm <ykmmn01263@gmail.com>
2026-01-15 00:31:08 +0900

    メッセージループを停止

--- src/ribot.py
+++ src/ribot.py
@@ -11,17 +11,5 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user}")
-    hello_loop.start()
-
-@tasks.loop(minutes=1)
-async def hello_loop():
-    # テスト用：全サーバーの最初のテキストチャンネルに送信
-    for guild in client.guilds:
-        for channel in guild.text_channels:
-            try:
-                await channel.send("Hello")
-                break
-            except Exception:
-                continue
 
 client.run(DISCORD_BOT_TOKEN)
